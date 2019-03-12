43d1ac6009d3c52ef8eb0dfc12ad147d3858a575
IN-BR-001 <IN-BR-001@IN-BR-001>
2019-03-12 14:56:18 +0100

    Julien : commit Admin, Client, Commande (model)

--- Projet_ecommerce/src/fr/adaming/model/Administrateur.java
+++ Projet_ecommerce/src/fr/adaming/model/Administrateur.java
@@ -1,13 +1,68 @@
 package fr.adaming.model;
 
-public class Administrateur {
-	
-	// Déclaration des attributs
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "administrateurs")
+public class Administrateur implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_a")
 	private int id;
 	private String username;
 	private String mdp;
-	
-	
-	
+
+	public Administrateur() {
+		super();
+	}
+
+	public Administrateur(String username, String mdp) {
+		super();
+		this.username = username;
+		this.mdp = mdp;
+	}
+
+	public Administrateur(int id, String username, String mdp) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.mdp = mdp;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getMdp() {
+		return mdp;
+	}
+
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+
+	@Override
+	public String toString() {
+		return "Administrateur [id=" + id + ", username=" + username + ", mdp=" + mdp + "]";
+	}
 
 }
--- Projet_ecommerce/src/fr/adaming/model/Client.java
+++ Projet_ecommerce/src/fr/adaming/model/Client.java
@@ -1,5 +1,104 @@
 package fr.adaming.model;
 
-public class Client {
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clients")
+public class Client implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_cl")
+	private int idClient;
+	private String nomClient;
+	private String adresse;
+	private String email;
+	private String tel;
+
+	@OneToMany(mappedBy="client")
+	private List<Commande> listeco;
+
+	public Client() {
+		super();
+	}
+
+	public Client(String nomClient, String adresse, String email, String tel) {
+		super();
+		this.nomClient = nomClient;
+		this.adresse = adresse;
+		this.email = email;
+		this.tel = tel;
+	}
+
+	public Client(int idClient, String nomClient, String adresse, String email, String tel) {
+		super();
+		this.idClient = idClient;
+		this.nomClient = nomClient;
+		this.adresse = adresse;
+		this.email = email;
+		this.tel = tel;
+	}
+
+	public int getIdClient() {
+		return idClient;
+	}
+
+	public void setIdClient(int idClient) {
+		this.idClient = idClient;
+	}
+
+	public String getNomClient() {
+		return nomClient;
+	}
+
+	public void setNomClient(String nomClient) {
+		this.nomClient = nomClient;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public List<Commande> getListeco() {
+		return listeco;
+	}
+
+	public void setListeco(List<Commande> listeco) {
+		this.listeco = listeco;
+	}
+
+	@Override
+	public String toString() {
+		return "Client [idClient=" + idClient + ", nomClient=" + nomClient + ", adresse=" + adresse + ", email=" + email
+				+ ", tel=" + tel + ", listeco=" + listeco + "]";
+	}
 
 }
