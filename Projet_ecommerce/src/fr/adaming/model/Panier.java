15a487e9bf15c986431aa716e49dadf7b3032adc
IN-BR-002 <IN-BR-002@IN-BR-002>
2019-03-13 11:47:43 +0100

    Eleonore: Commit Style

--- Projet_ecommerce/src/fr/adaming/model/Panier.java
+++ Projet_ecommerce/src/fr/adaming/model/Panier.java
@@ -4,8 +4,11 @@ import java.io.Serializable;
 import java.util.List;
 
 public class Panier implements Serializable{ 
+	
+	// Attributs
 	private List<LigneCommande> listelico;
 
+	// Constructeurs
 	public Panier() {
 		super();
 	}
@@ -15,6 +18,7 @@ public class Panier implements Serializable{
 		this.listelico = listelico;
 	}
 
+	// Getters et Setters
 	public List<LigneCommande> getListelico() {
 		return listelico;
 	}
